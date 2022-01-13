088f4fb4932b5e97bc2493df1b0c73e276e46e63
Justin Liu <jus030245@gmail.com>
2022-01-13 15:59:32 +0800

    adjust keyword

--- Web_frontend/src/GoogleQuery.java
+++ Web_frontend/src/GoogleQuery.java
@@ -40,7 +40,7 @@ public class GoogleQuery
 
 		this.searchKeyword = searchKeyword;
 
-		this.url = "http://www.google.com/search?q="+searchKeyword+"&oe=utf8&num=30";
+		this.url = "http://www.google.com/search?q=business+analytics+"+searchKeyword+"&oe=utf8&num=30";
 
 	}
 
